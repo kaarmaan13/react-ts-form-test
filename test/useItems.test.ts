4759645f183dca60368fa9d26de4f915ebef2524
Manuel Del Castillo <manuel.delcastillo@walmeric.com>
2024-01-31 15:18:48 +0100

    custom hook testing

---
+++ test/useItems.test.ts
@@ -0,0 +1,22 @@
+import { describe, expect, test } from 'vitest'
+import { renderHook, act } from '@testing-library/react'
+import { useItems } from '../src/hooks/useItems'
+
+describe('useItems hook', () => {
+  test('should add and remove items', () => {
+    const { result } = renderHook(() => useItems())
+
+    expect(result.current.items.length).toBe(0)
+    
+    act(() => {
+      result.current.addItem('Jugar a videojuegos 🎮')
+      result.current.addItem('Ir a correr 🏃‍♂️')
+    })
+    
+    act(() => {
+      result.current.removeItem(result.current.items[0].id)
+    })
+
+    expect(result.current.items.length).toBe(1)
+  })
+})
